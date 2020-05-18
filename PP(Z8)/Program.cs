36b4eea2b69a9aa6b49e0ffb7afa8f02e3f8d828
alina.timonina172372 <alina.timonina172372@gmail.com>
2020-05-18 17:14:57 +0300

    Добавьте файлы проекта.

---
+++ PP(Z8)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace PP_Z8_
+{
+    class Program
+    {
+        private static IEnumerable<double> GetNumbers(string input)
+        {
+            var matches = Regex.Matches(input, @"-?\d+(?:\.\d+)?", RegexOptions.Compiled);
+            return from Match match in matches select double.Parse(match.Value, CultureInfo.InvariantCulture);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строчку: ");
+            var text = Console.ReadLine();
+            var numbers = GetNumbers(text);
+            if (numbers.Any())
+                Console.Write("Max число в сообщении это: {0}", numbers.Max());
+            else
+                Console.Write("В сообщении нет числа!");
+            Console.ReadKey();
+        }
+    }
+}
